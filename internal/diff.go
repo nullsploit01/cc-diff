d83cce57dbb2206294c960bfb871758c7e79f5ff
nullsploit01 <harshaldharmik099@gmail.com>
2024-11-27 22:11:52 +0530

    add a function to find LCS

---
+++ internal/diff.go
@@ -0,0 +1,50 @@
+package internal
+
+// use Hunt-Szymanski algo to find LCS
+func FindLCS(a, b string) string {
+	positions := make(map[rune][]int)
+
+	for i, char := range b {
+		positions[char] = append(positions[char], i)
+	}
+
+	findLIS := func(indices []int) []int {
+		lis := []int{}
+
+		for _, index := range indices {
+			if len(lis) == 0 || index > lis[len(lis)-1] {
+				lis = append(lis, index)
+			} else {
+				l, r := 0, len(lis)-1
+				for l < r {
+					mid := (l + r) / 2
+					if lis[mid] >= index {
+						r = mid
+					} else {
+						l = mid + 1
+					}
+				}
+
+				lis[l] = index
+			}
+		}
+
+		return lis
+	}
+
+	indices := []int{}
+	for _, char := range a {
+		if idxList, exists := positions[char]; exists {
+			indices = append(indices, idxList...)
+		}
+	}
+
+	lisIndices := findLIS(indices)
+
+	lcs := make([]rune, len(lisIndices))
+	for i, idx := range lisIndices {
+		lcs[i] = rune(b[idx])
+	}
+
+	return string(lcs)
+}
